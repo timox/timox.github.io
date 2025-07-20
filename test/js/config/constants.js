a8449a07d9e11edab4449fc55ea9bef8568e77ae
timo <toxlady@gmail.com>
2025-07-20 03:45:29 +0200

    bordel strategie fix22124782

--- test/js/config/constants.js
+++ test/js/config/constants.js
@@ -156,6 +156,375 @@ export const STRATEGY_DATA = [
     "responsable": "Exploitation",
     "echeance": "2024-2025",
     "portee": "GSSI"
+  },
+  {
+    "id": 3,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Appliquer les dispositions organisationnelles des annexes 1 et 2",
+    "action": "Création et animation du réseau des correspondants informatiques",
+    "responsable": "CGSSI+ISI",
+    "echeance": "2024-2025",
+    "portee": "GSSI"
+  },
+  {
+    "id": 4,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Appliquer les dispositions organisationnelles des annexes 1 et 2",
+    "action": "Ouverture d'un guichet unique",
+    "responsable": "GSSI",
+    "echeance": "2024-2025",
+    "portee": "GSSI"
+  },
+  {
+    "id": 5,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Appliquer les dispositions organisationnelles des annexes 1 et 2",
+    "action": "Attribution des privilèges d'accès selon des critères respectant le principe du moindre privilège.",
+    "responsable": "Exploitation",
+    "echeance": "2024-2025",
+    "portee": "GSSI"
+  },
+  {
+    "id": 6,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Mise en conformité et durcissement de l'AD",
+    "responsable": "Exploitation",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 7,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Suivi du plan d'action de l'audit réseau pour disposer d'une architecture réseau simplifiée, segmentée, exploitable et maintenable",
+    "responsable": "Réseaux",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 8,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Mise en place d'un outil de suivi et anticipation des actions",
+    "responsable": "Réseaux",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 9,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Simplification des architectures : internet, intranet, extranet, serveurs de fichiers, serveur mail…",
+    "responsable": "Réseaux",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 10,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Sécurisation et homogénéisation des moyens d'accès distant (RDS, VPN, dotations en matériel portable)",
+    "responsable": "ISI+CGSSI",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 11,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Sécurisation et homogénéisation des moyens d'accès distant (RDS, VPN, dotations en matériel portable)",
+    "responsable": "Exploitation",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 12,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre en conformité les réseaux, logiciels et matériels",
+    "action": "Sécurisation et homogénéisation des moyens d'accès distant (RDS, VPN, dotations en matériel portable)",
+    "responsable": "ISI+RSSI",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 13,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique du SDIS",
+    "action": "Rédaction d'une charte utilisateur des systèmes d'information",
+    "responsable": "GSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 14,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique du SDIS",
+    "action": "Rédaction d'une charte spécifique pour les administrateurs système et les correspondants",
+    "responsable": "GSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 15,
+    "objectif": "Assurer le fonctionnement des systèmes d'information",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique du SDIS",
+    "action": "Élaboration d'un plan de sauvegarde des données",
+    "responsable": "GSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 16,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Établir la PSSI",
+    "action": "Réalisation d'un audit sécurité : conformité à NIS2",
+    "responsable": "ISI+RSSI+CGSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 17,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Établir la PSSI",
+    "action": "Rédaction d'une PSSI prenant en compte la réglementation NIS2 et sa transposition.",
+    "responsable": "ISI+RSSI+CGSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 18,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Appliquer la PSSI",
+    "action": "Mise en place de procédures et actions prenant en compte la PSSI",
+    "responsable": "GSSI+SDIS",
+    "echeance": "2025-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 19,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Surveiller les activités informatiques",
+    "action": "Amélioration des outils de supervision",
+    "responsable": "RSSI+ISI",
+    "echeance": "2024-2026",
+    "portee": "Générale"
+  },
+  {
+    "id": 20,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Surveiller les activités informatiques",
+    "action": "Mise en conformité de la logique d'exposition sur internet",
+    "responsable": "RSSI+ISI",
+    "echeance": "2024-2026",
+    "portee": "Générale"
+  },
+  {
+    "id": 21,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Indicateurs de sécurité",
+    "action": "Mise en place et partage d'indicateurs de sécurité",
+    "responsable": "RSSI + CGSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 22,
+    "objectif": "Garantir la sécurité des systèmes d'information",
+    "sous_objectif": "Indicateurs de sécurité",
+    "action": "Sensibilisation/formation à partir de ces indicateurs.",
+    "responsable": "RSSI + CGSSI",
+    "echeance": "2025",
+    "portee": "Générale"
+  },
+  {
+    "id": 23,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Pérenniser les outils à destination des métiers et utilisateurs",
+    "action": "Transmission des connaissances d'exploitation et développement d'intranet",
+    "responsable": "ISI",
+    "echeance": "2024-",
+    "portee": "Générale"
+  },
+  {
+    "id": 24,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Pérenniser les outils à destination des métiers et utilisateurs",
+    "action": "Aide à la maitrise d'ouvrage pour la continuité des outils et le support avec les fournisseurs de logiciels",
+    "responsable": "ISI",
+    "echeance": "2024-",
+    "portee": "Générale"
+  },
+  {
+    "id": 25,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Pérenniser les outils à destination des métiers et utilisateurs",
+    "action": "Mise en œuvre d'un outil de déploiement automatisé",
+    "responsable": "ISI",
+    "echeance": "2024-",
+    "portee": "Générale"
+  },
+  {
+    "id": 26,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Préciser les démarches de qualification et validation du GSSI (AMO)",
+    "action": "Document cadre (schéma directeur)",
+    "responsable": "ISI+CGSSI",
+    "echeance": "2024",
+    "portee": "Générale"
+  },
+  {
+    "id": 27,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Préciser les démarches de qualification et validation du GSSI (AMO)",
+    "action": "Réunions avec chefs de groupements",
+    "responsable": "CGSSI",
+    "echeance": "2024-",
+    "portee": "Générale"
+  },
+  {
+    "id": 28,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Organiser les modalités de saisines et le suivi des réponses du service aux sollicitations",
+    "action": "Simplification et optimisation du traitement des demandes avec guichet unique (pour l'instant GLPI et intranet)",
+    "responsable": "RSSI+exploitation",
+    "echeance": "2024-",
+    "portee": "Générale"
+  },
+  {
+    "id": 29,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Organiser les modalités de saisines et le suivi des réponses du service aux sollicitations",
+    "action": "Mise en place d'un inventaire automatisé en lien avec un outil de déploiement des matériels",
+    "responsable": "RSSI+exploitation",
+    "echeance": "2024-",
+    "portee": "Générale"
+  },
+  {
+    "id": 30,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Organiser les modalités de saisines et le suivi des réponses du service aux sollicitations",
+    "action": "Mise en place d'un inventaire automatisé en lien avec un outil de déploiement des matériels",
+    "responsable": "Exploitation",
+    "echeance": "2024",
+    "portee": "Générale"
+  },
+  {
+    "id": 31,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Mettre en place les solutions NexSIS-RRF",
+    "action": "Évaluer le retroplanning avec les grandes actions",
+    "responsable": "Équipe projet",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 32,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Mettre en place les solutions NexSIS-RRF",
+    "action": "Définir les budgets et échéances financières",
+    "responsable": "Équipe projet",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 33,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Mettre en place les solutions NexSIS-RRF",
+    "action": "Prévoir et accompagner les évolutions techniques",
+    "responsable": "Équipe projet",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 34,
+    "objectif": "Répondre aux demandes des métiers",
+    "sous_objectif": "Mettre en place les solutions NexSIS-RRF",
+    "action": "Former et tenir au fait les utilisateurs",
+    "responsable": "Équipe projet",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 35,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Bâtir les systèmes d'information de demain",
+    "action": "Évaluer l'existant : intranet, extranet, GED, GEC, SAE, logiciels, intégration",
+    "responsable": "Groupe de travail",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 36,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Bâtir les systèmes d'information de demain",
+    "action": "Benchmark dans les autres SIS",
+    "responsable": "Groupe de travail",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 37,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Bâtir les systèmes d'information de demain",
+    "action": "Proposer les outils de demain (informatique = cloud, outils de communication modernes, téléphonie)",
+    "responsable": "Groupe de travail",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 38,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Bâtir les systèmes d'information de demain",
+    "action": "Mettre en place les outils de demain (nouvel intranet, workflows, tableaux de bord, outils de pilotage, circulation de l'information…)",
+    "responsable": "Groupe de travail",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 39,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Bâtir les systèmes d'information de demain",
+    "action": "Prendre en compte le PCA (Cloud, infrastructures réseau, communication, etc.)",
+    "responsable": "Groupe de travail",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 40,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique",
+    "action": "Prendre en compte les évolutions validées suite aux travaux des groupes",
+    "responsable": "GSSI",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 41,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique",
+    "action": "Produire les plans de continuité et/ou de reprise de l'activité",
+    "responsable": "GSSI",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 42,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique",
+    "action": "Veiller à la cohérence du système, aux opérations de nettoyage et de documentation",
+    "responsable": "GSSI",
+    "echeance": "2024-2030",
+    "portee": "Générale"
+  },
+  {
+    "id": 43,
+    "objectif": "Assurer la transition vers les systèmes d'information de demain",
+    "sous_objectif": "Mettre à jour le schéma directeur informatique",
+    "action": "Inclure les nouveaux outils, les solutions émergentes",
+    "responsable": "GSSI",
+    "echeance": "2024-2030",
+    "portee": "Générale"
   }
 ];
 
