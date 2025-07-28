1fa73f594c3322d4d96c3686952176d4d83a9d2e
timo <toxlady@gmail.com>
2025-07-28 19:52:23 +0200

    fix modales

--- test/js/managers/ModalManager.js
+++ test/js/managers/ModalManager.js
@@ -64,7 +64,7 @@ export class ModalManager {
     const historyModalElement = document.getElementById('task-history-modal');
     if (historyModalElement) {
       this.historyModal = new bootstrap.Modal(historyModalElement, {
-        backdrop: true,
+        backdrop: false,
         keyboard: true,
         focus: true
       });
